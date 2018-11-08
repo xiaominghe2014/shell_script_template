7f81debd995c90b98221c7139bc98abc745f17e2
Ximena <>
2018-11-08 13:59:48 +0800

    add other method to clear directory

--- src/general/clear_dir.sh
+++ src/general/clear_dir.sh
@@ -3,6 +3,31 @@
 this shell script for clear directory. 
 !
 
+# only clear all files in directory
+clear_all_files() {
+    if [ $# -lt 1 ];then
+        echo "need directory argument" 1>&2
+        exit 1
+    fi
+    echo "$1"
+    _path="$1"
+    res_f=$(find "$_path" -type f  -exec rm -f {} \; 2>/dev/null)
+    echo $res_f
+}
+
+# clear any sub directories in directory
+clear_all_subdirs() {
+    if [ $# -lt 1 ];then
+        echo "need directory argument" 1>&2
+        exit 1
+    fi
+    echo "$1"
+    _path="$1"
+    res_d=$(find "$_path" -type d  -mindepth 1  -exec rm -rf {} \; 2>/dev/null)
+    echo $res_d
+}
+
+# clear any files include sub directories in directory
 clear_dir() {
     if [ $# -lt 1 ];then
         echo "need directory argument" 1>&2
@@ -10,10 +35,8 @@ clear_dir() {
     fi
     echo "$1"
     _path="$1"
-    res_f=$(find "$_path" -type f  -mindepth 1  -exec rm -f {} \; 2>/dev/null)
-    echo $res_f
-    res_d=$(find "$_path" -type d  -mindepth 1  -exec rm -rf {} \; 2>/dev/null)
-    echo $res_d
+    res=$(find "$_path" -xdev -mindepth 1 -exec rm -rf {} \; 2>/dev/null)
+    echo $res
 }
 
 clear_dir "$@"
